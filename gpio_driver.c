b891ac79c6a152ed91dd9265d22ca37de878f172
aamir <aamir@Aamir-VM.myguest.virtualbox.org>
2023-12-08 15:26:43 -0700

    Updated name of device

--- gpio_driver.c
+++ gpio_driver.c
@@ -145,7 +145,7 @@ static int __init etx_driver_init(void)
   }
  
   /*Creating device*/
-  if(IS_ERR(device_create(dev_class,NULL,dev,NULL,"etx_device"))){
+  if(IS_ERR(device_create(dev_class,NULL,dev,NULL,"buzzer_gpio"))){
     pr_err( "Cannot create the Device \n");
     goto r_device;
   }
